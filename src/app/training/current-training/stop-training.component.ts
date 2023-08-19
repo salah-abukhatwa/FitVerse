56f7dac56a37bf3cce92012222e247998f719f0c
salah-abukhatwa <salah.abukhatwa@gmail.com>
2023-08-19 12:39:58 +0300

    add training section and Dialog

---
+++ src/app/training/current-training/stop-training.component.ts
@@ -0,0 +1,15 @@
+import { Component ,Inject } from '@angular/core';
+import { inject } from '@angular/core/testing';
+import { MAT_DIALOG_DATA} from '@angular/material/dialog';
+
+
+@Component({
+  selector: 'app-stop-training',
+  templateUrl: './stop-training.component.html',
+  styleUrls: ['./stop-training.component.css']
+})
+export class StopTrainingComponent {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public passData: any){}
+
+}
--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class TrainingComponent {
 
+  ongoingTraining = false;
 }
